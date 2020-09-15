e482139be563954558fc336e4705d60d1f2490f2
Sudhir1997 <sudhawan2@gmail.com>
2020-09-15 22:14:05 +0530

    2 files modified, 2 added

---
+++ src/Register.jsx
@@ -0,0 +1,171 @@
+import React from "react";
+import axios from "axios";
+
+import {
+  FireworkSpinner,
+  SequenceSpinner,
+  RotateSpinner,
+  FlapperSpinner
+} from "react-spinners-kit";
+
+import "./cssmodules/register.css";
+
+
+
+import styled, { keyframes } from 'styled-components';
+import { bounce } from 'react-animations';
+
+export default class Register extends React.Component {
+  state = { display: null };
+
+  helperDisplay() {
+    setTimeout(() => {
+      this.setState({ display: true });
+    }, 3000);
+  }
+//Handler Functions 
+handlerCountry(event){
+  alert(event.target.value);
+
+} 
+
+
+
+
+
+
+
+  componentDidMount() {
+    this.helperDisplay.call(this);
+  }
+
+  render() {
+    const bounceAnimation = keyframes`${bounce}`;
+    const BouncyDiv = styled.div`
+  animation: 1s ${bounceAnimation};
+`;
+    if (this.state.display === null) {
+      return (
+        <div id="main">
+          <RotateSpinner size={90} color={"#123abc"} />
+        </div>
+      );
+    } else {
+      return (
+        <>
+       
+        <h2 id="heading">Register Form</h2>
+
+
+        <form>
+          <div class="form-group">
+            <label for="firstName">First Name</label>
+            <input
+              type="text"
+              class="form-control"
+              id="firstName"
+              placeholder="Enter First Name"
+              required
+              name="firstName"
+            />
+          </div>
+ 
+          <div class="form-group">
+            <label for="lastName">Last Name</label>
+            <input
+              type="text"
+              class="form-control"
+              id="lastName"
+              placeholder="Enter Last Name"
+              required
+              name="lastName"
+            />
+          </div>
+ 
+          <div class="form-row">
+ 
+            <div class="form-group col-md-4">
+              <label for="email">Email</label>
+              <input type="email" placeholder="Enter Email" class="form-control" id="email" name="email"  required/>
+            </div>
+ 
+            <div class="form-group col-md-4">
+              <label for="password">Password</label>
+              <input type="password" placeholder="Enter Password" class="form-control" id="password" name="password" required />
+            </div>
+ 
+             <div class="form-group col-md-4">
+              <label for="confirmPasssword">Confirm Password</label>
+              <input type="password"  placeholder="Confirm Password" class="form-control" id="confirmPassword" required />
+            </div>
+              
+          </div>
+
+
+          <div class="form-group">
+            <label for="inputAddress">Address</label>
+            <input
+              type="text"
+              class="form-control"
+              id="inputAddress"
+              placeholder="1234 Main St"
+              required
+            />
+          </div>
+
+          <div class="form-group">
+            <label for="inputAddress2">Address 2</label>
+            <input
+              type="text"
+              class="form-control"
+              id="inputAddress2"
+              placeholder="Apartment, studio, or floor"
+            />
+          </div>
+
+          <div class="form-row">
+            <div class="form-group col-md-3">
+              <label for="inputCity">City</label>
+              <input type="text" class="form-control" id="inputCity" />
+            </div>
+
+          <div class="form-group col-md-4">
+              <label for="inputState">Country</label>
+              <select onChange={this.handlerCountry} id="inputState" class="form-control">
+                <option selected>Choose...</option>
+                <option>India</option>
+                <option>Japan</option>
+                <option>China</option>
+              </select>
+            </div>
+
+            <div class="form-group col-md-2">
+              <label for="inputZip">Zip</label>
+              <input type="text" class="form-control" id="inputZip" />
+            </div>
+
+          </div>
+
+           <label class="radio-inline">
+            <input type="radio" name="optradio" value="Male"/>Male
+            </label>
+            <label class="radio-inline">
+           <input id="female" type="radio" name="optradio" value="Female"/>Female
+            </label>
+
+          <div class="form-group">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="gridCheck" />
+              <label class="form-check-label" for="gridCheck">
+                Check me out
+              </label>
+            </div>
+          </div>
+         <button type="button" class="btn btn-secondary btn-lg btn-block">Block level button</button>
+        </form>
+       
+        </>
+      );
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Register from './Register.jsx'
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Register />, document.getElementById("root"));
